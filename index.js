29170e6604a407e499af49c6b484ca406c7bc1af
john <john.d.leonetti@gmail.com>
2021-06-11 15:23:48 -0700

    commit tree fe6236d5cdf92f3f7662756b902b7c72dcf69e53

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your code here
+// create class breakfast
+// class has food and drink
+
+
+// create class lunch
+// has salad/ soup/ entree
+
+
+// create class dinner 
+// has salad/soup/entree/ _dessert(private property)
+
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner {
+    constructor(salad, soup, entree, _dessert) {
+        this.salad = salad,
+        this.soup = soup;
+        this.entree = entree;
+        this._dessert = _dessert;
+    }
+}
